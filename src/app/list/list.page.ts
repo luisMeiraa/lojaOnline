33b6403f008b07ec41d51a56f1b88bf1e91a4408
luismeira <luismeira@ipvc.pt>
2019-02-13 15:46:20 +0000

    fix button up

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,42 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild, } from '@angular/core';
+import { NavController, IonContent } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: 'list.page.html',
   styleUrls: ['list.page.scss']
 })
-export class ListPage implements OnInit {
-  public isSearchbarOpened = false;
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+export class ListPage {
+
+  
+
+  @ViewChild(IonContent) content: IonContent;
+  
+	public backToStart(): void {
+	  this.content.scrollToTop();
   }
+
+  public isSearchbarOpened = false;
+ 
 
   onSearch(event){
     console.log(event.target.value);
   }
-
+  
   ngOnInit() {
   }
+
+
+
+
+
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
