dd15590cea4eb1f6af812210723cf4ea8419ca0c
luismeira <luismeira@ipvc.pt>
2019-02-19 14:44:35 +0000

    ficha de artigo

--- src/app/ficha-artigo/ficha-artigo.page.ts
+++ src/app/ficha-artigo/ficha-artigo.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController, NavParams } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-ficha-artigo',
   templateUrl: './ficha-artigo.page.html',
   styleUrls: ['./ficha-artigo.page.scss'],
 })
 export class FichaArtigoPage implements OnInit {
-  private selectedItem: any;
-  constructor() { }
 
+  public artigo: any;
+  public nome: any;
+  public photo: any;
+  public preco: any;
+  constructor(public navCtrl: NavController, private activatedRoute: ActivatedRoute) { }
+
+  ionViewDidEnter() {
+    this.nome = this.activatedRoute.snapshot.paramMap.get("nome");
+    console.log("nome do artigo: ", this.nome);
+
+    this.photo = this.activatedRoute.snapshot.paramMap.get("photo");
+    console.log("photo do artigo: ", this.photo);
+
+    this.preco = this.activatedRoute.snapshot.paramMap.get("preco");
+    console.log("preco do artigo: ", this.preco);
+
+    this.artigo = new Object();
+
+    this.artigo.nome = this.nome;
+    this.artigo.photo = this.photo;
+    this.artigo.preco = this.preco;
+  }
   ngOnInit() {
   }
 
 }
+
+
+
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,6 +1,8 @@
 
-import { Component, ViewChild, } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, IonContent } from '@ionic/angular';
+import { Router } from '@angular/router'
+import { FichaArtigoPage } from '../ficha-artigo/ficha-artigo.page';
 
 
 @Component({
@@ -8,26 +10,62 @@ import { NavController, IonContent } from '@ionic/angular';
   templateUrl: 'list.page.html',
   styleUrls: ['list.page.scss']
 })
+
 export class ListPage {
 
-  
+  public artigos: Array<object> = [];
+
+  constructor(private router: Router, public navCtrl: NavController) {
+    this.artigos = [
+      {
+        "nome": 'bolo1',
+        "photo": 'bolo.jpg',
+        "preco": '10€'
+      },
+      {
+
+        "nome": 'mesa2',
+        "photo": 'mesa.jpg',
+        "preco": '12€'
+      }
+      ,
+      {
+        "nome": 'exemplo2',
+        "photo": 'bolo.jpg',
+        "preco": '16€'
+      }
+      ,
+      {
+        "nome": 'exemplo3',
+        "photo": 'mesa.jpg',
+        "preco": '15€'
+      },
+    ]
+  }
 
   @ViewChild(IonContent) content: IonContent;
-  
-	public backToStart(): void {
-	  this.content.scrollToTop();
+
+  public backToStart(): void {
+    this.content.scrollToTop();
   }
 
   public isSearchbarOpened = false;
- 
 
-  onSearch(event){
+
+  onSearch(event) {
     console.log(event.target.value);
   }
-  
+
   ngOnInit() {
   }
 
+
+
+  goToFichaArtigo(artigo) {
+    console.log(artigo);
+    this.router.navigate(['ficha-artigo', { nome: artigo.nome, photo: artigo.photo, preco: artigo.preco }]);
+
+  }
 
 
 
